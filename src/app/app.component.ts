fe37d1c722dd0e14fc11271d269b2d5dcc43196b
Sri Krishna <iamabhishek10920@gmail.com>
2024-10-05 22:53:21 +0530

    Added navbar with signin-register tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './home/components/header/header.component';
+import { CatnavigationComponent } from './home/components/catnavigation/catnavigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CatnavigationComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'e-store';
