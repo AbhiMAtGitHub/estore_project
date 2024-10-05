b63a944b5f8d0e8a2f483ba296a2864f3b67a612
Sri Krishna <iamabhishek10920@gmail.com>
2024-10-05 23:27:47 +0530

    Added 404 page and a side navigation bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './home/components/not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
