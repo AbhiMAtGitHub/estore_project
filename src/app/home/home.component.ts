b276424674e1cf7c2278f82b1549d5889519dc4a
Sri Krishna <iamabhishek10920@gmail.com>
2024-10-08 22:11:02 +0530

    Added ratings component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../home/components/header/header.component';
 import { CatnavigationComponent } from './components/catnavigation/catnavigation.component';
 import { SidenavigationComponent } from './components/sidenavigation/sidenavigation.component';
+import { ProductsComponent } from './components/products/products.component';
+import { RatingsComponent } from '../shared/components/ratings/ratings.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, CatnavigationComponent, SidenavigationComponent],
+  imports: [
+    HeaderComponent,
+    CatnavigationComponent,
+    SidenavigationComponent,
+    ProductsComponent,
+    RatingsComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
